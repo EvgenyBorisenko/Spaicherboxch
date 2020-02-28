14d7c74f272704e95b0d6a59fb78cecf8250dcd1
Yevgeniy Borisenko <yevgeniy.borisenko@nearshorepartner.ch>
2020-02-28 23:09:34 +0200

    Updates

---
+++ precache-manifest.fe517dbeabd06e8edadb47b9b4e4ba34.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed21ab6ec36ad7eabb0047bba701d7c",
+    "url": "/Spaicherboxch/index.html"
+  },
+  {
+    "revision": "0155074d06a04d078aa4",
+    "url": "/Spaicherboxch/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "296d5a0ac17292ba521b",
+    "url": "/Spaicherboxch/static/css/main.21953c31.chunk.css"
+  },
+  {
+    "revision": "0155074d06a04d078aa4",
+    "url": "/Spaicherboxch/static/js/2.710180b5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Spaicherboxch/static/js/2.710180b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "296d5a0ac17292ba521b",
+    "url": "/Spaicherboxch/static/js/main.7a77c977.chunk.js"
+  },
+  {
+    "revision": "cc9d0c7b9051ac48a802",
+    "url": "/Spaicherboxch/static/js/runtime-main.83a8da3b.js"
+  },
+  {
+    "revision": "b1a5c769c299052e09c8947eb88d813b",
+    "url": "/Spaicherboxch/static/media/Anleitungen_def.b1a5c769.png"
+  },
+  {
+    "revision": "88de5b0302ec7701b69cc76579663b21",
+    "url": "/Spaicherboxch/static/media/Datensicherheit, Tresor_smal.88de5b03.jpg"
+  },
+  {
+    "revision": "bc03469e11d3e4b721e47db88816f585",
+    "url": "/Spaicherboxch/static/media/Telefon_def.bc03469e.png"
+  },
+  {
+    "revision": "ba662db50d61fdaa5dc8ed9e5ef2910d",
+    "url": "/Spaicherboxch/static/media/Ticket_def.ba662db5.png"
+  },
+  {
+    "revision": "a9cad9e38dd006756aa83ec1d263f6fb",
+    "url": "/Spaicherboxch/static/media/bg_other.a9cad9e3.png"
+  },
+  {
+    "revision": "64db3485f3082d6ee1b89cc953833975",
+    "url": "/Spaicherboxch/static/media/business.64db3485.jpg"
+  },
+  {
+    "revision": "ed5201b0d9ea88eefbda5fb18a1bce1b",
+    "url": "/Spaicherboxch/static/media/business_small.ed5201b0.jpg"
+  },
+  {
+    "revision": "27d93d7c83bcbdd3d21c97c4194a4be1",
+    "url": "/Spaicherboxch/static/media/family.27d93d7c.jpg"
+  },
+  {
+    "revision": "25a13355c0f1cff58f74b5ca63b6ac4a",
+    "url": "/Spaicherboxch/static/media/family_small.25a13355.jpg"
+  },
+  {
+    "revision": "a6e4ab7fe665475104380637381991c4",
+    "url": "/Spaicherboxch/static/media/home.a6e4ab7f.jpg"
+  },
+  {
+    "revision": "3ee1616ebc3485634f03432e1a55bf34",
+    "url": "/Spaicherboxch/static/media/home_small.3ee1616e.jpg"
+  },
+  {
+    "revision": "89998731659da4a1ee61898b1b0167e2",
+    "url": "/Spaicherboxch/static/media/invoice-icon.89998731.png"
+  },
+  {
+    "revision": "865fa3852d99b8f80a07b7d123161a1f",
+    "url": "/Spaicherboxch/static/media/logo_Wuerfel_speicherbox_1_klein.865fa385.png"
+  },
+  {
+    "revision": "cf21228c62486f00b435ac8c57609a27",
+    "url": "/Spaicherboxch/static/media/post-e-finance-icon.cf21228c.png"
+  },
+  {
+    "revision": "cea354311bd6da085689479da36662b4",
+    "url": "/Spaicherboxch/static/media/post-finance-icon.cea35431.png"
+  },
+  {
+    "revision": "36329283bfe0d525d0325a010337e191",
+    "url": "/Spaicherboxch/static/media/reseller.36329283.jpg"
+  },
+  {
+    "revision": "bae71b34192d504f41d7edd9dcd47ce3",
+    "url": "/Spaicherboxch/static/media/reseller_small.bae71b34.jpg"
+  },
+  {
+    "revision": "df770f2ab6d557d645ffffadf9a76c3b",
+    "url": "/Spaicherboxch/static/media/swissmade.df770f2a.png"
+  },
+  {
+    "revision": "1f17fe7aaba113b56e5f2237ed6eaab7",
+    "url": "/Spaicherboxch/static/media/verein_small.1f17fe7a.jpg"
+  },
+  {
+    "revision": "c52aff27cceff30cdc72dfb37a280631",
+    "url": "/Spaicherboxch/static/media/vereine.c52aff27.jpg"
+  }
+]);
